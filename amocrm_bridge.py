bbe31a821a8eb5dff5acc3037b18d1d9a72d6178
asilbekxushvaqtov070-cell <asilbekxushvaqtov070@gmail.com>
2026-06-22 19:44:03 +0500

    port yangi

--- amocrm_bridge.py
+++ amocrm_bridge.py
@@ -63,4 +63,4 @@ async def receive_webhook(request: Request):
 
 if __name__ == "__main__":
     print("AmoCRM ulanish serveri ishga tushmoqda...")
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
